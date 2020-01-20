7a8d5f0ecb59fa3a80b0d4c5b01eb13486fa2d5b
yah01 <kagaminehuan@live.com>
2020-01-21 00:41:56 +0800

    Update cydown_test.go

--- cydown_test.go
+++ cydown_test.go
@@ -1,6 +1,24 @@
 package cydown
 
-import "testing"
-func mergeTest(t *testing.T) {
+import (
+	"fmt"
+	"testing"
+	"time"
+)
 
+func TestDownload(t *testing.T) {
+	task := NewTask("https://dl.google.com/go/go1.13.6.windows-amd64.msi")
+	go task.Download("")
+	finish := false
+	go func() {
+		var last int64
+		for !finish {
+			fmt.Printf("%v Bytes/%v Bytes - %v Bytes/s\t\r", task.Count(), task.Size, task.Count()-last)
+			last = task.Count()
+			time.Sleep(time.Second)
+		}
+	}()
+	Wait()
+	finish = true
+	time.Sleep(time.Second)
 }
