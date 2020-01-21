7d985793f35c99ac6280ac86aa735ed08e2142e3
yah01 <kagaminehuan@live.com>
2020-01-21 12:27:56 +0800

    Update cydown_test.go

--- cydown_test.go
+++ cydown_test.go
@@ -1,24 +1,28 @@
 package cydown
 
 import (
-	"fmt"
+	"log"
 	"testing"
 	"time"
 )
 
 func TestDownload(t *testing.T) {
+	//UseGlobalLocalProxy("1080")
 	task := NewTask("https://dl.google.com/go/go1.13.6.windows-amd64.msi")
+	log.Println("Downloading")
+	TaskCounter.Add(1)
 	go task.Download("")
-	finish := false
-	go func() {
-		var last int64
-		for !finish {
-			fmt.Printf("%v Bytes/%v Bytes - %v Bytes/s\t\r", task.Count(), task.Size, task.Count()-last)
-			last = task.Count()
-			time.Sleep(time.Second)
-		}
-	}()
+	//finish := false
+	//go func() {
+	//	var last int64
+	//	for !finish {
+	//		fmt.Printf("%v Bytes/%v Bytes - %v Bytes/s\t\r", task.Count(), task.Size, task.Count()-last)
+	//		last = task.Count()
+	//		time.Sleep(time.Second)
+	//	}
+	//}()
 	Wait()
-	finish = true
+	log.Println("All done")
+	//finish = true
 	time.Sleep(time.Second)
 }
